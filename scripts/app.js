4c50e1541cb94b80a796d8ef89066fbcd19fc193
Dany Mrushi <mrushidany@gmail.com>
2025-05-05 13:29:36 +0300

    Setting up the nav and mobile nav

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+    // Mobile nav toggle
+    $('#menuToggle').on('click', function() {
+        $('#mobileNav').toggleClass('hidden')
+    })
+
     $.ajax({
         url: 'http://numbersapi.com/1/30/date?json',
         dataType: 'json',
